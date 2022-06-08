196fa20bfcd3628efef086182dfe14a7fe76a706
Kartikeya <106214939+kartikeya999@users.noreply.github.com>
2022-06-08 19:22:24 +0530

    Update variables.tf

--- exercise_2/variables.tf
+++ exercise_2/variables.tf
@@ -16,7 +16,7 @@ variable "s3_bucket_region" {
 variable "s3_bucket_name" {
   type        = string
   description = "Name of the S3 bucket to be created used to store the remote state file"
-  default     = "kartikeya-lab-ex1"
+  default     = "kartikeya-lab-ex2"
   }
 
 variable "tag_env" {
